912f4a5e5bf46d7353e83e7ed38a6775c0b153fe
Dipak-Chauhan <deepakchauhan98790@gmail.com>
2026-05-31 13:15:46 +0530

    changed app name

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String appName = 'Ganne Music Downloader';
+  static const String appName = 'Ganne';
   static const String appTagline = 'The simplest music downloader';
   static const String defaultDownloadPath = '/sdcard/Music/Ganne/';
   static const int maxConcurrentDownloads = 3;
